ff3e2059f391ef8f915419773eb5455adffeb77c
Andres Mejia <160779799+andresemejia@users.noreply.github.com>
2026-03-01 16:39:12 -0500

    Create clean_silver.sql

---
+++ scripts/silver/clean_silver.sql
@@ -0,0 +1,31 @@
+-- Check for unwanted spaces
+-- Expectation: No Results
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_material_status,
+	cst_gender,
+	cst_create_date)
+SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER (TRIM(cst_material_status)) = 'S' THEN 'Single'
+	WHEN UPPER (TRIM(cst_material_status)) = 'M' THEN 'Married'
+	ELSE 'n/a'
+END cst_material_status, -- Normalize martial status values to readable format
+CASE WHEN UPPER (TRIM(cst_gender)) = 'F' THEN 'Female'
+	WHEN UPPER (TRIM(cst_gender)) = 'M' THEN 'Male'
+	ELSE 'n/a'
+END cst_gender, -- Normalize gender values to readable format
+cst_create_date
+FROM(
+	SELECT
+	*,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1 -- Select the most recent record per customer
