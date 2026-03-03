e20851dd5ea7c2aafb7b2bf585a9fe5279b11fe4
Andres Mejia <160779799+andresemejia@users.noreply.github.com>
2026-03-03 15:20:19 -0500

    Update insert_silver.crm_sales_details.sql

--- scripts/silver/insert_silver.crm_sales_details.sql
+++ scripts/silver/insert_silver.crm_sales_details.sql
@@ -26,10 +26,10 @@ END AS sls_due_dt,
 CASE WHEN sls_sales IS NULL OR sls_sales <=0 OR sls_sales != sls_quantity * ABS(sls_price)
 		THEN sls_quantity * ABS(sls_price)
 	ELSE sls_sales
-END AS sls_sls,
+END AS sls_sls, -- Recalculate sales if original value is missing or incorrect
 sls_quantity,
 CASE WHEN sls_price IS NULL OR sls_price <= 0
 		THEN sls_sales / NULLIF(sls_quantity,0)
-	ELSE sls_price
+	ELSE sls_price -- Derive price if original value is invalid
 END AS sls_price
 FROM bronze.crm_sales_details
