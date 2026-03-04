3eed840e632c8e1fd99a7a4708a4859f7ebbef21
Andres Mejia <160779799+andresemejia@users.noreply.github.com>
2026-03-04 12:31:47 -0500

    Create clean_bronze.erp_px_cat_g1v2.sql

---
+++ scripts/silver/erp/clean_bronze.erp_px_cat_g1v2.sql
@@ -0,0 +1,11 @@
+-- Check unwanted Spaces
+SELECT * FROM bronze.erp_px_cat_g1v2
+WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+cat,
+subcat,
+maintenance
+FROM
+bronze.erp_px_cat_g1v2
