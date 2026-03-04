6317c394b0d6898693718d010595c3adb860d8f8
Andres Mejia <160779799+andresemejia@users.noreply.github.com>
2026-03-04 11:45:34 -0500

    Create validate_silver.erp_cust_az12.sql

---
+++ scripts/silver/validate_silver.erp_cust_az12.sql
@@ -0,0 +1,13 @@
+-- Identify Out-of-Range Dates
+
+SELECT DISTINCT
+bdate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+-- Data Standardization & Consistency
+
+SELECT DISTINCT gen
+FROM silver.erp_cust_az12
+
+SELECT * FROM silver.erp_cust_az12
