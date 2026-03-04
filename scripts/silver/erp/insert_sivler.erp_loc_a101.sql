72d1f8e89f2605e74ad7ce45de24643de74b11cc
Andres Mejia <160779799+andresemejia@users.noreply.github.com>
2026-03-04 12:22:34 -0500

    Update insert_sivler.erp_loc_a101.sql

--- scripts/silver/erp/insert_sivler.erp_loc_a101.sql
+++ scripts/silver/erp/insert_sivler.erp_loc_a101.sql
@@ -8,5 +8,5 @@ CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
 	WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
 	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'N/A'
 	ELSE TRIM(cntry)
-END AS cntry
+END AS cntry -- Normalize and Handle missing or blank country codes
 FROM bronze.erp_loc_a101 
