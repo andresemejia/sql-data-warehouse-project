2d19132b1d3c315713c28f4e9a8accbe9301ad44
Andres Mejia <160779799+andresemejia@users.noreply.github.com>
2026-03-04 11:55:36 -0500

    Update insert_silver.erp_cust_az12.sql

--- scripts/silver/erp/insert_silver.erp_cust_az12.sql
+++ scripts/silver/erp/insert_silver.erp_cust_az12.sql
@@ -5,14 +5,14 @@ INSERT INTO silver.erp_cust_az12 (
 )
 
 SELECT
-CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- Remove 'NAS' prefix if present
 	ELSE cid
 END cid,
-CASE WHEN bdate > GETDATE() THEN NULL
+CASE WHEN bdate > GETDATE() THEN NULL -- Set future birthdates to NULL
 		ELSE bdate
 END AS bdate,
 CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
 	WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
 	ELSE 'N/A'
-END AS gen
+END AS gen -- Normalize gender values and handle unknown cases
 FROM bronze.erp_cust_az12;
